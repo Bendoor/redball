956e75f8490fd4372f4964af0f57862557bf1a75
Bendoor <31519877+Bendoor@users.noreply.github.com>
2017-10-22 20:47:33 +0300

    ss

---
+++ luchik/Assets/scripts/move.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+	Rigidbody2D ball;
+	public float speed1=5f;
+	public float speed2=7f;
+
+
+	// Use this for initialization
+	void Start () {
+		ball = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		if (Input.GetKeyDown (KeyCode.Space))
+		{
+			ball.AddForce (Vector2.up * speed1, ForceMode2D.Impulse);
+		}
+
+		float h = Input.GetAxis("Horizontal");
+		if (h != 0) 
+		{
+			ball.AddForce (Vector2.right * speed2 * h);
+		}
+	}
+}
